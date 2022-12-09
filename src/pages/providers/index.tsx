eac9663ac5988ed9a44cf259d9e13c3a4fb32af9
Gustav Stensson <103995817+gustavste@users.noreply.github.com>
2022-12-09 12:18:45 +0100

    extract text to mdx files

--- src/pages/providers/index.tsx
+++ src/pages/providers/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Layout from '@theme/Layout';
+
+import MDXContent from '@theme/MDXContent';
 import styles from './provider.module.css';
 import { usePluginData } from '@docusaurus/useGlobalData';
 import { TileSearch, TileItem } from '@site/src/components/TileSearch';
+import GameProviders from './_game-providers.mdx'
 
 
 export default function Providers() {
@@ -15,11 +18,9 @@ export default function Providers() {
         <div className={`container ${styles.mainPage}`}>
           <div className={styles.left}>
             <h1 className={`${styles.header}`}>Game provider</h1>
-            <div>
-              Discover game providers you can unlock by using Valkyrie as you open source aggregator.
-              All providers listed have implemented modules in valkyrie enabling you as a operator to integrate with minimal coding efforts.
-              All you need is agreements with the provider and setup your valkyrie instance to make all the games available.
-            </div>
+            <MDXContent>
+              <GameProviders />
+            </MDXContent>
           </div>
           <div className={styles.right}>
             <TileSearch items={providers} label="Search Provider" />
